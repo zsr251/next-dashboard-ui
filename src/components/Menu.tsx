8ab3b2c1a5c303dc59d19cb9e0ed8d6cc85e70d2
jasonzhu <zsr251@163.com>
2024-09-09 17:54:03 +0800

    init

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +116,23 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div>
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map((item) => (
+            <Link key={item.label} href={item.href} className="flex items-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        lamaSky:"#C3EBFA",
+        lamaSkyLight:"#EDF9FD",
+        lamaPurple:"#CFCEFF",
+        lamaPurpleLight:"#F1F0FF",
+        lamaYellow:"#FAE27C",
+        lamaYellowLight:"#FEFCE8",
+        lamaRed:"#FF817B",
+        lamaRedLight:"#FFEAE6",
+      }
     },
   },
   plugins: [],
